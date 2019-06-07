4fd3f087b69989260686ba591c8e1a503ac36200
User55@CAPSCI.LOCAL <User55@CAPSCI.LOCAL>
2019-06-07 14:02:42 -0500

    add DoNotEmail.js

---
+++ DoNotEmailScript/DoNotEmail/DoNotEmail.js
@@ -0,0 +1,10 @@
+/*
+ * 
+ *Client Script to prevent fulfillment emails from being sent to customer.
+ *Created 6/7/2019 by B-Rad
+ *Requires library_integration_functions.js
+ *Requires library_common_functions.js
+ * 
+ */
+
+
